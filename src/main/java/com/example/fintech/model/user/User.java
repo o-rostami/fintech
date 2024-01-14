7b72d7611305fb5789c578da36f84dab532c8728
Omid Rostami <o.rostami@mydigipay.com>
2024-01-14 23:42:13 +0330

    int: initial project for the first time

---
+++ src/main/java/com/example/fintech/model/user/User.java
@@ -0,0 +1,14 @@
+package com.example.fintech.model.user;
+
+
+import lombok.Data;
+
+@Data
+public class User {
+	private Integer id;
+
+	private String userName;
+
+	private String password;
+
+}
---
+++ src/test/java/com/example/fintech/FintechApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.fintech;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FintechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
